f959778ccd2e7ba9ef1e5c5638cac96c7dfaf15a
mariomi <55362526+mariomi@users.noreply.github.com>
2023-10-30 08:18:28 +0100

    hey

--- Pavan_Diego/JDBC/DBManager.java
+++ Pavan_Diego/JDBC/DBManager.java
@@ -19,6 +19,7 @@ public class DBManager implements AutoCloseable {
         String sql = "INSERT INTO data (ID, Name, Surname, age) VALUES (?, ?, ?, ?)";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             String ID = generateID(name, surname, age);
+            pstmt.setString(1, ID);
             pstmt.setString(2, name);
             pstmt.setString(3, surname);
             pstmt.setInt(4, age);
@@ -30,9 +31,10 @@ public class DBManager implements AutoCloseable {
         List<String> results = new ArrayList<>();
         String sql = "SELECT * FROM data";
         try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+                ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
-                results.add(rs.getString("ID") + ", " + rs.getString("Name") + ", " + rs.getString("Surname") + ", " + rs.getInt("age"));
+                results.add(rs.getString("ID") + ", " + rs.getString("Name") + ", " + rs.getString("Surname") + ", "
+                        + rs.getInt("age"));
             }
         }
         return results;
@@ -72,26 +74,89 @@ public class DBManager implements AutoCloseable {
     public double averageAge() throws SQLException {
         String sql = "SELECT AVG(age) FROM data";
         try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+                ResultSet rs = stmt.executeQuery(sql)) {
             if (rs.next()) {
                 return rs.getDouble(1);
             }
         }
         return 0;
     }
+
+    public int countStudentsAboveGivenAge(int ageThreshold) throws SQLException {
+        String sql = "SELECT COUNT(*) FROM data WHERE age > ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, ageThreshold);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        }
+        return 0;
+    }
+
+    public List<String> oldestStudents() throws SQLException {
+        List<String> results = new ArrayList<>();
+        String sql = "SELECT * FROM data WHERE age = (SELECT MAX(age) FROM data)";
+        try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                results.add(rs.getString("ID") + ", " + rs.getString("Name") + ", " + rs.getString("Surname") + ", "
+                        + rs.getInt("age"));
+            }
+        }
+        return results;
+    }
+    public List<String> studentsWithNameSequence(String sequence) throws SQLException {
+        List<String> results = new ArrayList<>();
+        String sql = "SELECT * FROM data WHERE Name LIKE ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, "%" + sequence + "%");
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                results.add(rs.getString("ID") + ", " + rs.getString("Name") + ", " + rs.getString("Surname") + ", " + rs.getInt("age"));
+            }
+        }
+        return results;
+    }
+
+    public int increaseAllStudentsAge() throws SQLException {
+        String sql = "UPDATE data SET age = age + 1";
+        try (Statement stmt = conn.createStatement()) {
+            return stmt.executeUpdate(sql);
+        }
+    }
+    public int copyToAlunniBack() throws SQLException {
+        String sql = "INSERT INTO AlunniBack SELECT * FROM data";
+        try (Statement stmt = conn.createStatement()) {
+            return stmt.executeUpdate(sql);
+        }
+    }
+
+    public int moveToAlunniOld(int ageThreshold) throws SQLException {
+        String copySql = "INSERT INTO AlunniOld SELECT * FROM data WHERE age = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(copySql)) {
+            pstmt.setInt(1, ageThreshold);
+            int copied = pstmt.executeUpdate();
+
+            String deleteSql = "DELETE FROM data WHERE age = ?";
+            conn.prepareStatement(deleteSql);
+            pstmt.setInt(1, ageThreshold);
+            pstmt.executeUpdate();
+            
+            return copied;
+        }
+    }
     private String generateID(String name, String surname, int age) {
         String namePart = name.length() >= 2 ? name.substring(0, 2) : name;
         String surnamePart = surname.length() >= 2 ? surname.substring(0, 2) : surname;
-    
+
         return namePart + surnamePart + age;
     }
 
-    // ... Other methods for the remaining functionalities ...
-
     @Override
     public void close() throws SQLException {
-       if (conn != null) {
-          conn.close();
-       }
+        if (conn != null) {
+            conn.close();
+        }
     }
 }
--- Pavan_Diego/JDBC/MainProgram.java
+++ Pavan_Diego/JDBC/MainProgram.java
@@ -13,11 +13,18 @@ public class MainProgram {
                     System.out.println("2 - Visualizza l'elenco");
                     System.out.println("3 - Modifica un elemento");
                     System.out.println("4 - Cancella un elemento");
-                    // ... Other options go here ...
+                    System.out.println("5 - Contare il numero di alunni con età maggiore di un certo valore");
+                    System.out.println("6 - Visualizzare l'età media");
+                    System.out.println("7 - Visualizzare gli alunni più vecchi");
+                    System.out.println("8 - Visualizzare gli alunni che hanno nel nome una sequenza data");
+                    System.out.println("9 - Aumentare l'età di tutti gli alunni di un anno");
+                    System.out.println("10 - Copiare nella tabella AlunniBack tutti i dati della tabella Alunni");
+                    System.out.println("11 - Spostare nella tabella AlunniOld tutti gli alunni di una certa età");
+        
                     System.out.println("99 - Esci");
 
                     int choice = scanner.nextInt();
-                    scanner.nextLine(); // Consume newline left-over
+                    scanner.nextLine();
 
                     switch (choice) {
                         case 1:
@@ -41,7 +48,7 @@ public class MainProgram {
                             }
                             break;
 
-                            case 3:
+                        case 3:
                             System.out.println("Inserisci ID dell'alunno da modificare:");
                             String oldID = scanner.nextLine();
                             System.out.println("Inserisci Nome:");
@@ -50,30 +57,71 @@ public class MainProgram {
                             surname = scanner.nextLine();
                             System.out.println("Inserisci età:");
                             age = scanner.nextInt();
-                            scanner.nextLine();  // consume the newline
-                            // First, delete the old record
+                            scanner.nextLine();
                             if (db.deleteElement(oldID) <= 0) {
                                 System.out.println("Errore durante la modifica.");
                                 break;
                             }
-                            // Now, insert the new record with the generated ID
                             if (db.insertElement(name, surname, age) > 0) {
                                 System.out.println("Elemento modificato con successo.");
                             } else {
                                 System.out.println("Errore durante la modifica.");
                             }
                             break;
-                        
+
                         case 4:
                             System.out.println("Inserisci ID dell'alunno da eliminare:");
-                            oldID  = scanner.nextLine();
+                            oldID = scanner.nextLine();
                             if (db.deleteElement(oldID) > 0) {
                                 System.out.println("Elemento eliminato con successo.");
                             } else {
                                 System.out.println("Errore durante l'eliminazione.");
                             }
                             break;
-                        // ... Other cases to handle the other functionalities ...
+                        case 5:
+                            System.out.println("Inserisci l'età minima:");
+                            int ageThreshold = scanner.nextInt();
+                            int count = db.countStudentsAboveGivenAge(ageThreshold);
+                            System.out.println("Numero di alunni con età maggiore di " + ageThreshold + ": " + count);
+                            break;
+
+                        case 6:
+                            double avgAge = db.averageAge();
+                            System.out.println("Età media degli alunni: " + avgAge);
+                            break;
+
+                        case 7:
+                            List<String> oldest = db.oldestStudents();
+                            System.out.println("Alunni più vecchi:");
+                            for (String student : oldest) {
+                                System.out.println(student);
+                            }
+                            case 8:
+                            System.out.println("Inserisci la sequenza da cercare nel nome:");
+                            String sequence = scanner.nextLine();
+                            List<String> matched = db.studentsWithNameSequence(sequence);
+                            System.out.println("Alunni che corrispondono:");
+                            for (String student : matched) {
+                                System.out.println(student);
+                            }
+                            break;
+        
+                        case 9:
+                            db.increaseAllStudentsAge();
+                            System.out.println("Età di tutti gli alunni aumentata di un anno.");
+                            break;
+        
+                        case 10:
+                            int copied = db.copyToAlunniBack();
+                            System.out.println(copied + " alunni copiati nella tabella AlunniBack.");
+                            break;
+        
+                        case 11:
+                            System.out.println("Inserisci l'età degli alunni da spostare in AlunniOld:");
+                            int ageThreshold1 = scanner.nextInt();
+                            int moved = db.moveToAlunniOld(ageThreshold1);
+                            System.out.println(moved + " alunni spostati nella tabella AlunniOld.");
+                            break;
 
                         case 99:
                             System.out.println("Arrivederci!");
